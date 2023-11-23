811f37f978eb75e359c2452630b670cbd71df371
Saverio <svscagn@gmail.com>
2023-11-23 17:56:58 +0100

    refine testing

--- test/csv.test.ts
+++ test/csv.test.ts
@@ -1,69 +1,79 @@
-import { readFileSync } from "fs";
-import { CSV } from "../src";
-import { expect, it } from "vitest";
+import { readFileSync, unlinkSync, writeFileSync } from "fs";
+import { CSV } from "../dist";
+import { afterAll, describe, expect, it } from "vitest";
 
-it("Correctly read a .csv file", () => {
-  const headers = [
-    "Index",
-    "Organization",
-    "Id",
-    "Name",
-    "Website",
-    "Country",
-    "Description",
-    "Founded",
-    "Industry",
-    "Number of employees"
-  ];
+describe("CSV", () => {
+  it("Correctly read a .csv file", () => {
+    const headers = [
+      "Index",
+      "Organization",
+      "Id",
+      "Name",
+      "Website",
+      "Country",
+      "Description",
+      "Founded",
+      "Industry",
+      "Number of employees"
+    ];
 
-  const csv100 = new CSV({ path: "test/100-lines.csv", headers });
-  expect(csv100.read().length).toEqual(100);
+    const csv100 = new CSV({ path: "test/100-lines.csv", headers });
+    expect(csv100.read().length).toEqual(100);
 
-  const csv1000 = new CSV({ path: "test/1000-lines.csv", headers });
-  expect(csv1000.read().length).toEqual(1000);
-});
-
-it("Create a .csv file with only the headers", () => {
-  const csv = new CSV({ path: "test/test1.csv", headers: ["name", "age"] });
-
-  expect(csv.read()).toEqual([]);
-});
-
-it("Handle .csv files with no headers", () => {
-  const csv = new CSV({
-    path: "test/empty.csv",
-    headers: ["will", "be", "inserted"]
+    const csv1000 = new CSV({ path: "test/1000-lines.csv", headers });
+    expect(csv1000.read().length).toEqual(1000);
   });
 
-  expect(readFileSync("test/empty.csv", "utf-8")).toEqual("will,be,inserted");
-});
+  it("Create a .csv file with only the headers", () => {
+    const csv = new CSV({ path: "test/test1.csv", headers: ["name", "age"] });
 
-it("Handle writing to a .csv file", () => {
-  const csv = new CSV({ path: "test/write.csv", headers: ["name", "age"] });
+    expect(csv.read()).toEqual([]);
+  });
 
-  csv.write({ name: "John", age: 20 });
+  it("Handle .csv files with no headers", () => {
+    const csv = new CSV({
+      path: "test/empty.csv",
+      headers: ["will", "be", "inserted"]
+    });
 
-  expect(csv.read()).toEqual([{ name: "John", age: 20 }]);
+    expect(readFileSync("test/empty.csv", "utf-8")).toEqual("will,be,inserted");
+  });
 
-  csv.write({ name: "Jane", age: 21 });
+  it("Handle writing to a .csv file", () => {
+    const csv = new CSV({ path: "test/write.csv", headers: ["name", "age"] });
 
-  expect(csv.read()).toEqual([
-    { name: "John", age: 20 },
-    { name: "Jane", age: 21 }
-  ]);
+    csv.write({ name: "John", age: 20 });
 
-  csv.write({ name: "John", age: 20 });
+    expect(csv.read()).toEqual([{ name: "John", age: 20 }]);
 
-  expect(csv.read()).toEqual([
-    { name: "John", age: 20 },
-    { name: "Jane", age: 21 }
-  ]);
+    csv.write({ name: "Jane", age: 21 });
 
-  csv.write({ age: 35, name: "Nancy" });
+    expect(csv.read()).toEqual([
+      { name: "John", age: 20 },
+      { name: "Jane", age: 21 }
+    ]);
 
-  expect(csv.read()).toEqual([
-    { name: "John", age: 20 },
-    { name: "Jane", age: 21 },
-    { name: "Nancy", age: 35 }
-  ]);
+    csv.write({ name: "John", age: 20 });
+
+    expect(csv.read()).toEqual([
+      { name: "John", age: 20 },
+      { name: "Jane", age: 21 }
+    ]);
+
+    csv.write({ age: 35, name: "Nancy" });
+
+    expect(csv.read()).toEqual([
+      { name: "John", age: 20 },
+      { name: "Jane", age: 21 },
+      { name: "Nancy", age: 35 }
+    ]);
+  });
+
+  afterAll(() => {
+    unlinkSync("test/test1.csv");
+    unlinkSync("test/write.csv");
+    unlinkSync("test/empty.csv");
+
+    writeFileSync("test/empty.csv", "");
+  });
 });
